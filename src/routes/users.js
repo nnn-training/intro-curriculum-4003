3d8d2560cf5c00f5df49905fa0926810b2316999
mana2216 <ms00lx.221b@gmail.com>
2024-09-14 10:33:05 +0900

    C:/Program Files/Git/users のページにおける認証を実装

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,9 @@
 const { Hono } = require("hono");
+const middleware = require("../middleware");
 
 const app = new Hono();
+
+app.use(middleware.ensureAuthenticated());
 
 app.get("/", (c) => {
   return c.text("respond with a resource");
